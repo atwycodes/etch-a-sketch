8e38209f0e63d7f5f5583e1ebcda047a886f20f9
Andrew Tan <andrewwytan@gmail.com>
2022-08-21 22:48:44 +0800

    added functionality to create grid (row and column of n x n size). todo: fit it within confines of grid-box

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ function makeGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         let row = document.createElement("div");
         row.setAttribute("class", "row");
-        row.textContent = `${i+1}`;
+        // row.textContent = `R${i+1}`;
         gridBox.appendChild(row);
         // console.log(i);
         // console.log(row.className);
+        for (let i = 0; i < gridSize; i++) {
+            let column = document.createElement("div");
+            column.setAttribute("class", "column");
+            // column.textContent = `C${i+1}`;
+            row.appendChild(column);
+        }
     }
 }
