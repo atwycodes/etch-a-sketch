fb7ec87c9d9e38ad916581cf034f29005a8f1400
Andrew Tan <andrewwytan@gmail.com>
2022-08-31 15:45:58 +0800

    added JS code for dynamic grid sizing based on gridBox size & gridSize input

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-let gridBox = document.querySelector("#grid-box");
-
-
 function makeGrid(gridSize) {
-    for (let i = 0; i < gridSize; i++) {
-        let row = document.createElement("div");
-        row.setAttribute("class", "row");
-        // row.textContent = `R${i+1}`;
-        gridBox.appendChild(row);
-        // console.log(i);
-        // console.log(row.className);
+    let gridDimensions = 300/gridSize;
+    let gridBox = document.querySelector("#grid-box");
         for (let i = 0; i < gridSize; i++) {
-            let cell = document.createElement("div");
-            cell.setAttribute("class", "cell");
-            // cell.textContent = `C${i+1}`;
-            row.appendChild(cell);
+            let row = document.createElement('div');
+            row.setAttribute('class', 'row');
+            gridBox.appendChild(row);
+            
+            for (let i = 0; i < gridSize; i++) {
+                let cell = document.createElement('div');
+                cell.setAttribute('class', 'cell');
+                cell.style.width = `${gridDimensions}` + `px`
+                cell.style.height = `${gridDimensions}` + `px`
+                row.appendChild(cell);
         }
+    
     }
 }
+
+makeGrid(4);
