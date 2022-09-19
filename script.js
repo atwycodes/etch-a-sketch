833f568f06a4e09652f19d81133d2e014057c8c6
Andrew Tan <andrewwytan@gmail.com>
2022-09-20 00:39:08 +0800

    added clear button functionality

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-//EVENT LISTENERS
-
 // GENERATE GRID & ATTACH EVENT LISTENERS
+let gridSize = 16
+
+let gridBox = document.querySelector('#grid-box');
 function makeGrid(gridSize) {
-    let gridBox = document.querySelector('#grid-box');
     let gridDimensions = 500/gridSize;
         for (let i = 0; i < gridSize; i++) {
             let row = document.createElement('div');
@@ -15,13 +15,13 @@ function makeGrid(gridSize) {
                 cell.style.width = `${gridDimensions}` + `px`
                 cell.style.height = `${gridDimensions}` + `px`
                 row.appendChild(cell);
-                // attach event listener in each cell
+                // attach event listener in each cell for colouring
                 cell.addEventListener('mouseover', () => colourCell(cell))
         }
     }
 }
 
-//COLOUR CELL
+//COLOURING CELL
 let userColour = document.querySelector('#user-colour')
 userColour.addEventListener('input', () => console.log(userColour.value));
 
@@ -29,6 +29,20 @@ function colourCell(cell) {
     cell.style.backgroundColor = `${userColour.value}`
 }
 
-// TEST AREA
-makeGrid(20);
+//CLEAR DRAWING GRID
+let clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', () => clearGrid(gridSize));
 
+function clearGrid(gridSize) {
+    const collection = document.getElementsByClassName('cell');     
+    let n = gridSize * gridSize;
+    console.log(n);
+    let clearColor = `#fdf5e6`;
+
+    for (let i = 0; i < n; i++) {
+        collection[i].style.backgroundColor = clearColor;
+    }
+}
+
+// TEST AREA
+makeGrid(gridSize);
