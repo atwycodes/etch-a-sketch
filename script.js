30cc8e3bfd5b1cf04c3a7254e9400ddc29213da3
Andrew Tan <andrewwytan@gmail.com>
2022-10-01 10:28:49 +0800

    added clearing of grid everytime makeGrid is invoked. testing new logic for colouring

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // GENERATE GRID & ATTACH EVENT LISTENERS
 let gridSize = 16
-
+let totalCells = gridSize * gridSize;
 let gridBox = document.querySelector('#grid-box');
+
+const collection = document.getElementsByClassName('cell');
+
 function makeGrid(gridSize) {
+    // clear grid area when invoked again
+    while (gridBox.firstChild) {
+        gridBox.removeChild(gridBox.lastChild);
+    }
     let gridDimensions = 500/gridSize;
     for (let i = 0; i < gridSize; i++) {
         let row = document.createElement('div');
@@ -16,38 +23,49 @@ function makeGrid(gridSize) {
             cell.style.height = `${gridDimensions}` + `px`
             row.appendChild(cell);
             // attach event listener in each cell for colouring
-            cell.addEventListener('mouseover', () => colourCell(cell))
+            cell.addEventListener('mouseover', () => colourCell(cell));
         }
     }
 }
 
-//COLOURING CELL
-let userColour = document.querySelector('#user-colour')
-userColour.addEventListener('input', () => console.log(userColour.value));
+//COLOUR & ERASER MODE
+let colourMode = document.querySelector('#user-colour')
+colourMode.addEventListener('input', () => console.log(colourMode.value));
 
 function colourCell(cell) {
-    cell.style.backgroundColor = `${userColour.value}`
+    cell.style.backgroundColor = `${colourMode.value}`
 }
 
-//ERASER MODE
+let eraserMode = document.querySelector('#eraser-mode')
+eraserMode.addEventListener('input', () => console.log('eraser mode activated'));
+
 function eraseCell(cell) {
     cell.style.backgroundColor = '#fdf5e6'
 }
 
 //CLEAR DRAWING GRID
 let clearButton = document.querySelector('#clear')
-clearButton.addEventListener('click', () => clearGrid(gridSize));
+clearButton.addEventListener('click', () => clearGrid(totalCells));
 
-function clearGrid(gridSize) {
-    const collection = document.getElementsByClassName('cell');     
-    let n = gridSize * gridSize;
-    console.log(n);
+function clearGrid(totalCells) {
     let clearColor = `#fdf5e6`;
-
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < totalCells; i++) {
         collection[i].style.backgroundColor = clearColor;
     }
 }
 
 // TEST AREA
 makeGrid(gridSize);
+
+let currentMode = 'default'
+
+// Conditions to change color
+if (currentMode === 'default') {
+    let currentColour = `${colourMode.value}`; 
+} else if (currentMode === 'colour') {
+    let currentColour = `${colourMode.value}`; 
+} else if (currentMode === 'random') {
+    let currentColour = ``; 
+} else if (currentMode === 'eraser') {
+    let currentColour = `#fdf5e6`;
+}
