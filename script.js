1e874de3fdadc59fcd20b2c84e10671e8bc8d407
Andrew Tan <andrewwytan@gmail.com>
2022-10-04 23:58:21 +0800

    completed logic for active states. colour,eraser and clear button working. to add random mode logic.

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // GENERATE GRID & ATTACH EVENT LISTENERS
-let gridSize = 16
-const totalCells = gridSize * gridSize;
+let gridSize = document.querySelector('#grid-size');
+gridSize.addEventListener('input', () => makeGrid(gridSize.value))
 const gridBox = document.querySelector('#grid-box');
 const collection = document.getElementsByClassName('cell');
+makeGrid(16);
 
 function makeGrid(gridSize) {
     // clear grid area when invoked again
@@ -27,17 +28,26 @@ function makeGrid(gridSize) {
     }
 }
 
-//DEFINING THE MODES
-let currentMode = 'default'
+//CLEAR DRAWING GRID
+const clearButton = document.querySelector('#clear')
+clearButton.addEventListener('click', () => clearGrid());
 
+function clearGrid() {
+    let totalCells = gridSize.value * gridSize.value;
+    for (let i = 0; i < totalCells; i++) {
+        collection[i].style.backgroundColor = `#fdf5e6`;
+    }
+}
+
+//DEFINING THE MODES
 const colourMode = document.querySelector('#user-colour');
-colourMode.addEventListener('click', () => activateMode(colourMode))
+colourMode.addEventListener('click', () => activateMode(colourMode,eraserMode,randomMode))
 
 const eraserMode = document.querySelector('#eraser-mode');
-eraserMode.addEventListener('click', () => activateMode(eraserMode));
+eraserMode.addEventListener('click', () => activateMode(eraserMode,colourMode,randomMode));
 
 const randomMode = document.querySelector('#random-mode');
-randomMode.addEventListener('click', () => activateMode(randomMode));
+randomMode.addEventListener('click', () => activateMode(randomMode,eraserMode,colourMode));
 
 function activateMode(selectedMode,otherMode1,otherMode2) {
      selectedMode.classList.add('active');
@@ -45,30 +55,15 @@ function activateMode(selectedMode,otherMode1,otherMode2) {
      otherMode2.classList.remove('active');
 }
 
-// pending active state logic
 function colourCell(cell) {
-    if (currentMode === 'default') {
+    if (colourMode.className === 'active') {
         cell.style.backgroundColor = `${colourMode.value}`; 
-    } else if (currentMode === 'colour') {
-        cell.style.backgroundColor = `${colourMode.value}`; 
-    } else if (randomMode === 'Random Mode') {
-        let randomColour = 
+    } else if (eraserMode.className === 'btn-input active') {
+        let randomColour = ``;
         cell.style.backgroundColor = `${randomColour}`;
-    } else if (eraserMode === 'Eraser Mode') {
+    } else if (randomMode.className === 'btn-input active') {
         cell.style.backgroundColor = `#fdf5e6`;
     }
 }
 
-//CLEAR DRAWING GRID
-const clearButton = document.querySelector('#clear')
-clearButton.addEventListener('click', () => clearGrid(totalCells));
-
-function clearGrid(totalCells) {
-    let clearColor = `#fdf5e6`;
-    for (let i = 0; i < totalCells; i++) {
-        collection[i].style.backgroundColor = clearColor;
-    }
-}
-
 // TEST AREA
-makeGrid(gridSize);
